72d74c79f3e42ab24ce1147867135bb4a5416f20
Mehmet Sıddık Albayrak <ms.albayrak@hotmail.com>
2021-01-30 21:28:03 +0300

    16 Ocak 3. Derse kadar olan kodlar

--- DegerveReferansTipler/Program.cs
+++ DegerveReferansTipler/Program.cs
@@ -6,9 +6,24 @@ namespace DegerVeReferansTipler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("deneme");
+            //design pattern?? - solid yazılım geliştirme prensipleri??
+            /*
+             * 	Not: Csharp da int, decimal, float, double, bool = bunlar değer tiptir. Array, class ve interface referans tiptir. 
+             * 	referans tiplerin değerleri heap te tutulur. 
+             * 
+             * */
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            //sayi 1 in değeri burada 30 olarak kalır. Çünkü int değer tiptir. 
 
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            //sayilar1[0] elemanı 999 olarak değişir. Çünkü buradaki array referans tiptir. burada sayilar1 heap teki sayilar2 nin referans değerlerini alır.
+ 
 
         }
     }
