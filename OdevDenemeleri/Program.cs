a137aa87a581e3c7eda260469a18c1261a08a86b
Mehmet Sıddık Albayrak <ms.albayrak@hotmail.com>
2021-02-14 00:23:22 +0300

    ödev deneme commit eklendi

--- OdevDenemeleri/Program.cs
+++ OdevDenemeleri/Program.cs
@@ -6,7 +6,39 @@ namespace OdevDenemeleri
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+
+            int day = rnd.Next(1,7);
+
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Pazartesi");
+                    break;
+                case 2:
+                    Console.WriteLine("Salı");
+                    break;
+                case 3:
+                    Console.WriteLine("Çarşamba");
+                    break;
+                case 4:
+                    Console.WriteLine("Perşembe");
+                    break;
+                case 5:
+                    Console.WriteLine("Cuma");
+                    break;
+                case 6:
+                    Console.WriteLine("Cumartesi");
+                    break;
+                case 7:
+                    Console.WriteLine("Pazar");
+                    break;
+
+                default:
+                    Console.WriteLine("Lütfen 1-7 arasında bir sayı giriniz");
+                    break;
+            }
         }
     }
 }
